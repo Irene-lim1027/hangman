226dd1321329c1635270984620fddde262b3f9d5
Irene-Lim <irenelim1027@gmail.com>
2021-01-12 21:19:26 -0800

    adding

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 console.log('loaded scripts');
 
-const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G','H','I','J',
+'K','L','M','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-const WORDS = ['GAB'];
+const WORDS = ['apples','pears','bananas','watermelon','tomato','lemon','strawberry','grapes','lime','cherry','blueberry','peach'];
 const selectedWord = WORDS[0];
 
 let guessedLetters = [];
@@ -40,3 +41,4 @@ function guessLetter(letter) {
     }
   }
 }
+  
